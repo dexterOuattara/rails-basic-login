9db5c4d1c168b01ca1690ef3e1853317fcdcb9fb
dexterOuattara <31624715+dexterOuattara@users.noreply.github.com>
2021-02-10 23:04:04 +0000

    remove custom slug
    remove custom slug for now

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -92,6 +92,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description, :slug, :image , :user_id, :category_id)
+      params.require(:post).permit(:title, :description, :image , :user_id, :category_id)
     end
 end
